5327e23e3da0794440326df376dcd1e8787efca6
Sascha Zeller <zeller.dev@gmail.com>
2019-11-28 14:22:37 +0100

    Confluence repository scenario

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,20 @@ repositories {
 	mavenCentral()
 	jcenter()
 	mavenLocal()
+	maven {
+		url = uri("https://packages.atlassian.com/mvn/maven-external")
+	}
+	maven {
+		// This is only used if not found in mavenLocal
+		url = uri("https://gilbert.informatik.uni-stuttgart.de/api/v4/projects/328/packages/maven")
+		credentials(HttpHeaderCredentials::class.java) {
+			name = "Job-Token"
+			value = System.getenv("CI_JOB_TOKEN")
+		}
+		authentication {
+			this.create("header", HttpHeaderAuthentication::class.java)
+		}
+	}
 }
 
 tasks.withType<Test> {
@@ -22,6 +36,7 @@ dependencies {
 	testRuntime("org.livingdoc:livingdoc-junit-engine:${project.version}")
 	testRuntime("org.livingdoc:livingdoc-repository-file:${project.version}")
 	testRuntime("org.livingdoc:livingdoc-repository-rest:${project.version}")
+	testRuntime("org.livingdoc:livingdoc-repository-confluence:${project.version}")
 
 	testImplementation("org.livingdoc:livingdoc-api:${project.version}")
 	testImplementation("ch.qos.logback:logback-classic:1.2.3")
